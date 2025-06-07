e791c59bec4a8b968bd65aeaf54537e47decc9a2
hahaharrywu <mr.wuhr@gmail.com>
2025-06-06 23:08:06 -0700

    Added the trend line view

--- BouBou/ViewControllers/ProfileViewController.swift
+++ BouBou/ViewControllers/ProfileViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import SwiftUI
+import Charts
 
 class ProfileViewController: UIViewController {
 
     
     @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var chartView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,46 @@ class ProfileViewController: UIViewController {
         if avatarImageView.image == nil {
                 avatarImageView.image = UIImage(systemName: "photo")
         }
+        
+        let data = generateLastHalfYearData()
+        let swiftUIView = TrendChartView(data: data)
+        let hostingController = UIHostingController(rootView: swiftUIView)
+
+        addChild(hostingController)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        hostingController.view.backgroundColor = .clear
+        chartView.backgroundColor = .clear
+        chartView.addSubview(hostingController.view)
+
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: chartView.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: chartView.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: chartView.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: chartView.trailingAnchor)
+        ])
+
+        hostingController.didMove(toParent: self)
     }
+    
+    func generateLastHalfYearData() -> [DataPoint] {
+        let grades: [Double] = [
+            1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8,
+            2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8,
+            1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
+        ]
+
+        var data: [DataPoint] = []
+        let calendar = Calendar.current
+        let today = Date()
+
+        for i in 0..<24 {
+            if let date = calendar.date(byAdding: .day, value: -7 * (23 - i), to: today) {
+                data.append(DataPoint(date: date, grade: grades[i]))
+            }
+        }
+        return data
+    }
+
     
 
     override func viewDidLayoutSubviews() {
---
+++ BouBou/Views/TrendChartView.swift
@@ -0,0 +1,67 @@
+//
+//  TrendChartView.swift
+//  BouBou
+//
+//  Created by Hongrui Wu  on 6/6/25.
+//
+
+import SwiftUI
+import Charts
+
+struct DataPoint: Identifiable {
+    let id = UUID()
+    let date: Date
+    let grade: Double
+}
+
+struct TrendChartView: View {
+    let data: [DataPoint]
+
+    var body: some View {
+        Chart {
+            ForEach(data) { point in
+                LineMark(
+                    x: .value("Week", point.date),
+                    y: .value("Grade", point.grade)
+                )
+            }
+            
+            
+            // y axis
+            RuleMark(x: .value("Start", data.first?.date ?? Date()))
+                .foregroundStyle(.white)
+                .lineStyle(StrokeStyle(lineWidth: 2))
+
+            // x axis
+            RuleMark(y: .value("Zero", 0.0))
+                .foregroundStyle(.white)
+                .lineStyle(StrokeStyle(lineWidth: 2))
+        
+        }
+        
+        
+
+        
+        .chartXAxis {
+            AxisMarks(preset: .aligned, values: .stride(by: .weekOfYear)) { _ in
+                AxisGridLine().foregroundStyle(.clear)
+                AxisTick()
+            }
+        }
+        .chartYAxis {
+            AxisMarks(position: .leading) { _ in
+                AxisGridLine().foregroundStyle(.clear)
+                AxisTick()
+                AxisValueLabel()
+                    .font(.system(size: 16, weight: .semibold))
+                    .foregroundStyle(.white)
+            }
+        }
+
+
+
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.zero)
+
+    }
+}
